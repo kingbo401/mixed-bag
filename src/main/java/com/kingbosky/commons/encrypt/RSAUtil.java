cce24b4f167fa37822f33c93c66bc38c7473b1fc
tianqiongbo <tianqiongbo@pwrd.com>
2016-10-18 18:46:59 +0800

    优化

--- src/main/java/com/kingbosky/commons/encrypt/RSAUtil.java
+++ src/main/java/com/kingbosky/commons/encrypt/RSAUtil.java
@@ -30,11 +30,11 @@ public class RSAUtil {
 	 */
 	public static final String KEY_ALGORITHM = "RSA";
 
-	/** */
-	/**
-	 * 签名算法
-	 */
-	public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+//	/** */
+//	/**
+//	 * 签名算法
+//	 */
+//	public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
 
 	/** */
 	/**
@@ -86,21 +86,22 @@ public class RSAUtil {
 	 * <p>
 	 * 用私钥对信息生成数字签名
 	 * </p>
-	 * 
+	 * @param algorithm 
+	 * 				签名算法
 	 * @param data
 	 *            已加密数据
 	 * @param privateKey
 	 *            私钥(BASE64编码)
-	 * 
+	 * @param 
 	 * @return
 	 * @throws Exception
 	 */
-	public static String sign(byte[] data, String privateKey) throws Exception {
+	public static String sign(String algorithm, byte[] data, String privateKey) throws Exception {
 		byte[] keyBytes = Base64.decodeBase64(privateKey);
 		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
 		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
 		PrivateKey privateK = keyFactory.generatePrivate(pkcs8KeySpec);
-		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		Signature signature = Signature.getInstance(algorithm);
 		signature.initSign(privateK);
 		signature.update(data);
 		return Base64.encodeBase64String(signature.sign());
@@ -111,7 +112,8 @@ public class RSAUtil {
 	 * <p>
 	 * 校验数字签名
 	 * </p>
-	 * 
+	 * @param algorithm 
+	 * 				签名算法
 	 * @param data
 	 *            已加密数据
 	 * @param publicKey
@@ -123,13 +125,13 @@ public class RSAUtil {
 	 * @throws Exception
 	 * 
 	 */
-	public static boolean verify(byte[] data, String publicKey, String sign)
+	public static boolean verify(String algorithm, byte[] data, String publicKey, String sign)
 			throws Exception {
 		byte[] keyBytes = Base64.decodeBase64(publicKey);
 		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
 		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
 		PublicKey publicK = keyFactory.generatePublic(keySpec);
-		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		Signature signature = Signature.getInstance(algorithm);
 		signature.initVerify(publicK);
 		signature.update(data);
 		return signature.verify(Base64.decodeBase64(sign));
