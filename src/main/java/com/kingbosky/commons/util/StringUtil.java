dd9a8b63ebe2d14ee5481a2e1761440fcd7fcbfc
tianqiongbo <tianqiongbo@pwrd.com>
2016-11-18 10:03:02 +0800

    支持过滤emoji表情符

--- src/main/java/com/kingbosky/commons/util/StringUtil.java
+++ src/main/java/com/kingbosky/commons/util/StringUtil.java
@@ -3,9 +3,8 @@ package com.kingbosky.commons.util;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -232,17 +231,59 @@ public class StringUtil {
 		return ToStringBuilder.reflectionToString(obj, ToStringStyle.SHORT_PREFIX_STYLE);
 	}
 	
-	public static String replaceEmoji(String source, String s) {
-		if (source != null) {
-			Pattern emoji = Pattern.compile("[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]",
-					Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
-			Matcher emojiMatcher = emoji.matcher(source);
-			if (emojiMatcher.find()) {
-				source = emojiMatcher.replaceAll(s);
-				return source;
-			}
-			return source;
+	public static boolean containsEmoji(String source) {
+		if (StringUtils.isBlank(source)) {
+			return false;
 		}
-		return source;
+		int len = source.length();
+		for (int i = 0; i < len; i++) {
+			char codePoint = source.charAt(i);
+			if (isEmojiCharacter(codePoint)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean isEmojiCharacter(char codePoint) {
+		 return (codePoint >= 0x2600 && codePoint <= 0x27BF) // 杂项符号与符号字体
+	                || codePoint == 0x303D
+	                || codePoint == 0x2049
+	                || codePoint == 0x203C
+	                || (codePoint >= 0x2000 && codePoint <= 0x200F)//
+	                || (codePoint >= 0x2028 && codePoint <= 0x202F)//
+	                || codePoint == 0x205F //
+	                || (codePoint >= 0x2065 && codePoint <= 0x206F)//
+	                /* 标点符号占用区域 */
+	                || (codePoint >= 0x2100 && codePoint <= 0x214F)// 字母符号
+	                || (codePoint >= 0x2300 && codePoint <= 0x23FF)// 各种技术符号
+	                || (codePoint >= 0x2B00 && codePoint <= 0x2BFF)// 箭头A
+	                || (codePoint >= 0x2900 && codePoint <= 0x297F)// 箭头B
+	                || (codePoint >= 0x3200 && codePoint <= 0x32FF)// 中文符号
+	                || (codePoint >= 0xD800 && codePoint <= 0xDFFF)// 高低位替代符保留区域
+	                || (codePoint >= 0xE000 && codePoint <= 0xF8FF)// 私有保留区域
+	                || (codePoint >= 0xFE00 && codePoint <= 0xFE0F)// 变异选择器
+	                || codePoint >= 0x10000; // Plane在第二平面以上的，char都不可以存，全部都转
+	}
+
+	/**
+	 * 过滤emoji 或者 其他非文字类型的字符
+	 * @param source
+	 * @return
+	 */
+	public static String filterEmoji(String source) {
+		if (StringUtils.isBlank(source)) return "";
+		if (!containsEmoji(source)) {
+			return source;// 如果不包含，直接返回
+		}
+		StringBuilder buf = new StringBuilder(source.length());
+		int len = source.length();
+		for (int i = 0; i < len; i++) {
+			char codePoint = source.charAt(i);
+			if (!isEmojiCharacter(codePoint)) {
+				buf.append(codePoint);
+			}
+		}
+		return buf.toString();
 	}
 }
