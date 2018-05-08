3c39e8994fb4dc924ce568aeafe7936c4b3f9e3b
qiongbo.tqb <qiongbo.tqb@alibaba-inc.com>
2018-05-08 16:22:53 +0800

    排序参数添加防sql注入逻辑

--- src/main/java/com/kingbo401/commons/model/param/OrderParam.java
+++ src/main/java/com/kingbo401/commons/model/param/OrderParam.java
@@ -1,20 +1,50 @@
 package com.kingbo401.commons.model.param;
 
+import java.util.regex.Pattern;
+
 import com.kingbo401.commons.model.BasePojo;
 
 public class OrderParam extends BasePojo{
-	private String orderBy;
-	private boolean asc = true;
-	public String getOrderBy() {
-		return orderBy;
+	private static final Pattern FIELD_PATTERN = Pattern.compile("[a-zA-Z0-9_\\-\\.]+");//防止sql注入
+	private String orderByField;
+	private String sort = "asc";
+	public String getOrderByField() {
+		checkField();
+		return orderByField;
 	}
-	public void setOrderBy(String orderBy) {
-		this.orderBy = orderBy;
+	public void setOrderByField(String orderByField) {
+		this.orderByField = orderByField;
+		checkField();
 	}
-	public boolean isAsc() {
-		return asc;
+	
+	public String getSort() {
+		checkSort();
+		return sort;
 	}
-	public void setAsc(boolean asc) {
-		this.asc = asc;
+	
+	public void setSort(String sort) {
+		this.sort = sort;
+		checkSort();
+	}
+	
+	private void checkField(){
+		if(orderByField == null || orderByField.isEmpty()){
+			return;
+		}
+		if(!FIELD_PATTERN.matcher(orderByField).matches()){
+			throw new IllegalArgumentException("orderByField illegal:" + orderByField);
+		}
+	}
+	
+	private void checkSort(){
+		if(orderByField == null || orderByField.isEmpty()){
+			return;
+		}
+		if(sort == null || sort.isEmpty()){
+			throw new IllegalArgumentException("sort cannot null");
+		}
+		if(!sort.equalsIgnoreCase("asc") && !sort.equalsIgnoreCase("desc")){
+			throw new IllegalArgumentException("sort illegal:" + sort);
+		}
 	}
 }
