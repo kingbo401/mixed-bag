d15a7ea201df068f2baf9fadca0d9c0cd6a1f0a2
qiongbo.tqb <qiongbo.tqb@alibaba-inc.com>
2018-05-25 17:04:34 +0800

    CollectionUtil新增把集合以某个属性为key转换成map的功能

--- src/main/java/com/kingbo401/commons/util/CollectionUtil.java
+++ src/main/java/com/kingbo401/commons/util/CollectionUtil.java
@@ -1,7 +1,15 @@
 package com.kingbo401.commons.util;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+
+import com.kingbo401.commons.exception.MixedBagException;
 
 /**
  * 集合工具类
@@ -45,5 +53,28 @@ public class CollectionUtil {
      */
     public static boolean isNotEmpty(Map<?,?> coll) {
     	return !isEmpty(coll);
+    }
+    
+    public static <T> Map<Object, T> toMap(Collection<T> coll, String fieldName){
+		Map<Object, T> map = new HashMap<Object, T>();
+		if(CollectionUtils.isEmpty(coll)){
+			return map;
+		}
+		Assert.hasText(fieldName, "fieldName不能为空");
+		try{
+			for(T item : coll){
+				Class<? extends Object> clazz = item.getClass();
+				PropertyDescriptor pd = new PropertyDescriptor(fieldName, clazz);
+				Method readMethod = pd.getReadMethod();
+				map.put(readMethod.invoke(item), item);
+			}
+		}catch (Exception e) {
+			throw new MixedBagException(e);
+		}
+		return map;
+	} 
+    
+    public static <T> Map<Object, T> toIdMap(Collection<T> coll){
+    	return toMap(coll, "id");
     }
 }
