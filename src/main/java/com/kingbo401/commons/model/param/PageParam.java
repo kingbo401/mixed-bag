5f7950bca60823232e81f44bb0a8c844151de7ff
qiongbo.tqb <qiongbo.tqb@alibaba-inc.com>
2018-06-08 20:39:16 +0800

    删除无用代码

--- src/main/java/com/kingbo401/commons/model/param/PageParam.java
+++ src/main/java/com/kingbo401/commons/model/param/PageParam.java
@@ -1,14 +1,14 @@
 package com.kingbo401.commons.model.param;
 
-public class PageParam extends OrderParam{
+public class PageParam extends OrderParam {
 	private int pageNum = 1;
 	private int pageSize = 10;
-	
+	private int maxPageSize = 1000;
 	/**
 	 * 是否返回总条数
 	 */
 	private boolean returnTotalCount = true;
-	
+
 	public PageParam() {
 	}
 
@@ -17,25 +17,31 @@ public class PageParam extends OrderParam{
 		this.pageNum = pageNum;
 		this.pageSize = pageSize;
 	}
-	
+
 	public int getPageNum() {
 		return pageNum;
 	}
+
 	public void setPageNum(int pageNum) {
 		this.pageNum = pageNum;
 	}
+
 	public int getPageSize() {
+		if (pageSize > maxPageSize) {
+			pageSize = maxPageSize;
+		}
 		return pageSize;
 	}
+
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
-	
-	public int getOffset(){
-		if(pageNum < 1){
+
+	public int getOffset() {
+		if (pageNum < 1) {
 			pageNum = 1;
 		}
-		if(pageSize < 1){
+		if (pageSize < 1) {
 			pageSize = 10;
 		}
 		return (pageNum - 1) * pageSize;
@@ -47,5 +53,9 @@ public class PageParam extends OrderParam{
 
 	public void setReturnTotalCount(boolean returnTotalCount) {
 		this.returnTotalCount = returnTotalCount;
+	}
+	
+	public void initMaxPageSize(int maxPageSize){
+		this.maxPageSize = maxPageSize;
 	}
 }
