f515aef5542f92fe60ff78e075d84c2f736b7799
tianqiongbo195 <tianqiongbo195@hellobike.com>
2020-10-05 13:03:24 +0800

    up

--- src/main/java/com/kingbo401/commons/util/BeanMapper.java
+++ src/main/java/com/kingbo401/commons/util/BeanMapper.java
@@ -24,17 +24,6 @@ public class BeanMapper {
     private static final MapperFacade MAPPER_FACADE_DEFAULT = MAPPER_FACTORY_DEFAULT.getMapperFacade();
 
     /**
-     * Mapper工厂,空字段不复制
-     */
-    private static final MapperFactory MAPPER_FACTORY_NOT_NULL = new DefaultMapperFactory.Builder()
-            .mapNulls(false).build();
-
-    /**
-     * Mapper实例,空字段不复制
-     */
-    private static final MapperFacade MAPPER_FACADE_NOT_NULL = MAPPER_FACTORY_NOT_NULL.getMapperFacade();
-
-    /**
      * 映射实体
      *
      * @param toClass 映射类对象
@@ -42,26 +31,10 @@ public class BeanMapper {
      * @return 映射类对象
      */
     public static <E, T> E map(Class<E> toClass, T data) {
-        return map(toClass, data, true);
-    }
-
-    /**
-     * 映射实体
-     *
-     * @param toClass 映射类对象
-     * @param data    数据（对象）
-     * @param mapNull 是否复制空字段
-     * @return 映射类对象
-     */
-    public static <E, T> E map(Class<E> toClass, T data, boolean mapNull) {
-        if(data == null){
+    	if(data == null){
             return null;
         }
-        if (mapNull){
-            return MAPPER_FACADE_DEFAULT.map(data, toClass);
-        } else {
-            return MAPPER_FACADE_NOT_NULL.map(data, toClass);
-        }
+        return MAPPER_FACADE_DEFAULT.map(data, toClass);
     }
 
     /**
@@ -72,24 +45,9 @@ public class BeanMapper {
      * @return 映射类对象
      */
     public static <E, T> List<E> mapAsList(Class<E> toClass, Collection<T> data) {
-        return mapAsList(toClass, data, true);
-    }
-
-    /**
-     * 映射集合
-     *
-     * @param toClass 映射类对象
-     * @param data    数据（集合）
-     * @return 映射类对象
-     */
-    public static <E, T> List<E> mapAsList(Class<E> toClass, Collection<T> data, boolean mapNull) {
-        if (data == null || data.isEmpty()){
-            return null;
-        }
-        if (mapNull){
-            return MAPPER_FACADE_DEFAULT.mapAsList(data, toClass);
-        } else {
-            return MAPPER_FACADE_NOT_NULL.mapAsList(data, toClass);
-        }
+    	 if (data == null || data.isEmpty()){
+             return null;
+         }
+    	 return MAPPER_FACADE_DEFAULT.mapAsList(data, toClass);
     }
 }
